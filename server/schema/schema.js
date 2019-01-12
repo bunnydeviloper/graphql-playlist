a3631bdfc8ec46053120a3c12b861de59b1c73b1
Sophia Mai <phuongmai001@gmail.com>
2019-01-12 14:42:31 -0800

    return the correct data when querying

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -23,6 +23,7 @@ const BookType = new GraphQLObjectType({
       resolve(parent, args) {
         // console.log(parent);
         // return _.find(authors, { id: parent.authorid });
+        return Author.findById( parent.authorId );
       },
     },
   }),
@@ -39,6 +40,7 @@ const AuthorType = new GraphQLObjectType({
       resolve(parent, args) {
         // return _.filter(books, { authorid: parent.id });
         // use .filter instead of .find b/c result is an array of multiple items
+        return Book.find({ authorId: parent.id });
       },
     },
   }),
@@ -53,6 +55,7 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // code to get data from db/other source
         // return _.find(books, { id: args.id });
+        return Book.findById( args.id );
       },
     },
     author: {
@@ -61,18 +64,21 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // code to get data from db/other source
         // return _.find(authors, { id: args.id });
+        return Author.findById( args.id );
       },
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
         // return books;
+        return Book.find({});
       },
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
         // return authors;
+        return Author.find({});
       },
     },
   },
