1cce94e6de065db3792f3f3d41aeff37e5a9c8c3
Sophia Mai <phuongmai001@gmail.com>
2019-01-12 15:40:26 -0800

    make a BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import BookList from './components/BookList';
 
 class App extends Component {
   render() {
     return (
       <div id="main">
         <h1>Bookshelf App With GraphQL</h1>
+        <BookList />
       </div>
     );
   }
