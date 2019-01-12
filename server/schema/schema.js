8c00b7306c6ce9f02187373c9f026429215d1df1
Sophia Mai <phuongmai001@gmail.com>
2019-01-12 13:27:22 -0800

    add new author to mongo db instance

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -78,6 +78,27 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        let author = new Author({
+          name: args.name,
+          age: args.age,
+        });
+        return author.save(); // save new author to db and return data back
+      },
+    },
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: Mutation,
 });
