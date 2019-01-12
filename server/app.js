7551468a385757586746a5a277afed7da0da2560
Sophia Mai <phuongmai001@gmail.com>
2019-01-12 11:38:46 -0800

    connect to mongoose Mlab db

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+
 const schema = require('./schema/schema');
 
 const app = express();
+
+mongoose.connect('mongodb://sophie:test123@ds149414.mlab.com:49414/bookshelf-graphql', { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('Connected to Mlab database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
