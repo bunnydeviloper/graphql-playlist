d57f915b26b40443f548b5b99f90f62aa32ae5df
Sophia Mai <phuongmai001@gmail.com>
2019-01-12 14:21:18 -0800

    add new book to mongo db instance, link with authorId

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -95,6 +95,22 @@ const Mutation = new GraphQLObjectType({
         return author.save(); // save new author to db and return data back
       },
     },
+    addBook: {
+      type: BookType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        authorId: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId,
+        });
+        return book.save(); // save new book to db and return data back
+      },
+    },
   },
 });
 
